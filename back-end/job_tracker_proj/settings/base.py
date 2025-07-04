152cf4c2cbbdf4b18a751a1ea9ece6f0b63d4024
roger <rogerdelagarza121212@gmail.com>
2025-07-03 20:25:53 -0500

    updated base

--- back-end/job_tracker_proj/settings/base.py
+++ back-end/job_tracker_proj/settings/base.py
@@ -38,6 +38,21 @@ CSRF_TRUSTED_ORIGINS  = config("DJANGO_CSRF_TRUSTED_ORIGINS", default="").split(
 TOKEN_COOKIE_NAME = "token"
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
+
 
 # Application definition
 
